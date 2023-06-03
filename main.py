b735eee315b2c37852029b23ef20cdc8eaab1796
Marcin Kostka <mkostka5@gmail.com>
2023-06-03 10:59:08 +0200

    added testy

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ def stozek(r, l, h):
     objetosc= round(1/3*math.pi*r**2*h,2)
     return(pole, objetosc)
 
+# testy
+
+assert (stozek(10, 10, 10)) == (628.3192653589794, 1047.2)
+assert (stozek(5, 5, 5)) == (157.07981633974484, 130.9)
+assert (stozek(2, 2, 2)) == (25.136370614359173, 8.38)
