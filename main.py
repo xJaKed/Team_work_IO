224dff41edf497184eb0659315c31a621d658569
Jakub Kedzierski <76137@student.ue.poznan.pl>
2023-06-03 09:34:12 +0200

    Testy funkcji

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ def szescian(krawedz_a):
     objetosc = krawedz_a ** (3)
     pole = 6 * krawedz_a**(2)
     return objetosc, pole
+
+# testy
+
+assert szescian(5) == (125, 150)
+assert szescian(1) == (1, 6)
+assert szescian(10) == (1000, 600)
