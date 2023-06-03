a23e52d08dd712440f9536e679d61765ded2398c
Jakub Kedzierski <76137@student.ue.poznan.pl>
2023-06-03 09:21:18 +0200

    Zaokrąglenie wyniku

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
 
+# Zaokrąglenie objętości kuli do 2 miejsc po przecinku
+
 def obj_kuli(r):
-    objetosc=4/3*math.pi*r**3
+    objetosc=round(4/3*math.pi*r**3,2)
     return(objetosc)
