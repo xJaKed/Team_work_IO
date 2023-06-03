197c764c068d1948f5044e9da612e080a4a7c3cf
Jakub Kedzierski <76137@student.ue.poznan.pl>
2023-06-03 11:02:51 +0200

    Testy ostroslup

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def ostroslup(pp, pb, h):
     pole = pp + pb
     objetosc = 1/3 * pp * h
     return pole, objetosc
+
+# testy
+
+assert (ostroslup(20,10,5)) == (30, 33.33333333333333)
+assert (ostroslup(50,20,10)) == (70, 166.66666666666663)
+assert (ostroslup(25,15,12)) == (40, 99.99999999999999)
