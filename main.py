8b4883e4ca7905daacc6dfd9d4a9f9e0bfea771e
Marcin Kostka <mkostka5@gmail.com>
2023-06-03 10:55:28 +0200

    added ostroslup

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 import math
 
-def szescian(krawedz_a):
-    objetosc = krawedz_a ** (3)
-    pole = 6 * krawedz_a**(2)
-    return objetosc, pole
-
-# Zaokrąglenie objętości kuli do 2 miejsc po przecinku
-
-def obj_kuli(r):
-    objetosc=round(4/3*math.pi*r**3,2)
-    return(objetosc)
-
-# testy
-
-assert (obj_kuli(10)) == 4188.79
-assert (obj_kuli(11)) == 5575.28
-assert (obj_kuli(12)) == 7238.23
-assert (obj_kuli(13)) == 9202.77
-assert (obj_kuli(14)) == 11494.04
-
-assert szescian(5) == (125, 150)
-assert szescian(1) == (1, 6)
-assert szescian(10) == (1000, 600)
+def ostroslup(pp, pb, h):
+    pole = pp + pb
+    objetosc = 1/3 * pp * h
+    return pole, objetosc
