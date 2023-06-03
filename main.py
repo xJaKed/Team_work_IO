6db8cc31e4ed7dea0ae69964201c08f9ed891edc
Marcin Kostka <mkostka5@gmail.com>
2023-06-03 09:36:44 +0200

    added tests

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import math
 def obj_kuli(r):
     objetosc=round(4/3*math.pi*r**3,2)
     return(objetosc)
+
+# test
+
+assert (obj_kuli(10)) == 4188.79
+assert (obj_kuli(11)) == 5575.28
+assert (obj_kuli(12)) == 7238.23
+assert (obj_kuli(13)) == 9202.77
+assert (obj_kuli(14)) == 11494.04
